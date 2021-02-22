b3f41d40ac5f645f819dc5d759f80ff9c8f94af4
Rico-Jose <imbangjoserico@gmail.com>
2021-02-22 13:53:24 +0800

    create layout

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -11,6 +11,7 @@ namespace DutchTreat.Controllers
     {
         public IActionResult Index()
         {
+            //throw new InvalidOperationException();
             return View();
         }
     }
