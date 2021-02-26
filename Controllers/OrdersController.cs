bcaeb17b375e2a1006ea525fa09bdd474fb62834
Rico-Jose <imbangjoserico@gmail.com>
2021-02-26 14:15:36 +0800

    validation and view models

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 ﻿using DutchTreat.Data;
 using DutchTreat.Data.Entities;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -54,15 +55,43 @@ namespace DutchTreat.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]Order model)
+        public IActionResult Post([FromBody]OrderViewModel model)
         {
             // add it to the db
             try
             {
-                _repository.AddEntity(model);
-                if (_repository.SaveAll())
+                var newOrder = new Order
                 {
-                    return Created($"/api/orders/{model.Id}", model);
+                    OrderDate = model.OrderDate,
+                    OrderNumber = model.OrderNumber,
+                    Id = model.OrderId
+                };
+
+                if (newOrder.OrderDate == DateTime.MinValue)
+                {
+                    newOrder.OrderDate = DateTime.Now;
+                }
+
+
+
+                if (ModelState.IsValid)
+                {
+                    _repository.AddEntity(newOrder);
+                    if (_repository.SaveAll())
+                    {
+                        var vm = new OrderViewModel()
+                        {
+                            OrderId = newOrder.Id,
+                            OrderDate = newOrder.OrderDate,
+                            OrderNumber = newOrder.OrderNumber
+                        };
+
+                        return Created($"/api/orders/{vm.OrderId}", vm);
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
                 }
             }
             catch (Exception ex)
